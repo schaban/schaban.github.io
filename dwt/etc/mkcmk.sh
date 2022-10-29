daa101d989c157f6125980d2fb7ab1b7c006966b
schaban <sergey.chaban@gmail.com>
2022-10-29 12:27:27 +0300

    + dwt cmake generator;

---
+++ dwt/etc/mkcmk.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+CMK_FILE="CMakeLists.txt"
+EXE_NAME="${1:-draw-walkthrough}"
+
+echo "cmake_minimum_required(VERSION 3.4)" > $CMK_FILE
+echo "set (CMAKE_CXX_STANDARD 11)" >> $CMK_FILE
+
+echo "project($EXE_NAME)" >> $CMK_FILE
+echo "include_directories(inc core)" >> $CMK_FILE
+echo "find_library(lib-pthread pthread)" >> $CMK_FILE
+echo "find_library(lib-dl dl)" >> $CMK_FILE
+echo "find_library(lib-X11 X11)" >> $CMK_FILE
+echo "add_definitions(-DX11)" >> $CMK_FILE
+
+echo "add_executable(" >> $CMK_FILE
+echo "$EXE_NAME" >> $CMK_FILE
+./build.sh srcs $CMK_FILE append
+echo ")" >> $CMK_FILE
+
+echo "target_link_libraries(" >> $CMK_FILE
+echo "$EXE_NAME \${lib-pthread} \${lib-dl} \${lib-X11}" >> $CMK_FILE
+echo ")" >> $CMK_FILE
+
+SHDR_DST="\${CMAKE_CURRENT_SOURCE_DIR}/data/simple_ogl"
+SHDR_SRC="\${CMAKE_CURRENT_SOURCE_DIR}/src/shaders"
+echo "add_custom_command(" >> $CMK_FILE
+echo "TARGET $EXE_NAME POST_BUILD" >> $CMK_FILE
+echo "COMMAND \${CMAKE_COMMAND} -E make_directory $SHDR_DST" >> $CMK_FILE
+echo ")" >> $CMK_FILE
+echo "add_custom_command(" >> $CMK_FILE
+echo "TARGET $EXE_NAME POST_BUILD" >> $CMK_FILE
+echo "COMMAND \${CMAKE_COMMAND} -E copy $SHDR_SRC/* $SHDR_DST" >> $CMK_FILE
+echo ")" >> $CMK_FILE
